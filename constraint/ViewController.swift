1b237aaa38df6d595eb3dace66bce7753f5a5083
ykGoose <yokogus@gmail.com>
2020-10-14 19:32:06 +0900

    Initial Commit

---
+++ constraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  constraint
+//
+//  Created by Владимир Падусов on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
